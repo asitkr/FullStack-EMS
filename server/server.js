4431d0572d6438a4faeab64e556721c5f15bf02f
asitkr <krsinhasaurabh@gmail.com>
2026-07-07 18:13:29 +0530

    Leave API's done

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import authRouter from "./routes/authRoutes.js";
 import employeesRouter from "./routes/employeeRoutes.js";
 import profileRouter from "./routes/profileRoutes.js";
 import attendanceRouter from "./routes/attendanceRoutes.js";
+import leaveRouter from "./routes/leaveRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -23,6 +24,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/employees", employeesRouter);
 app.use("/api/profile", profileRouter);
 app.use("/api/attendance", attendanceRouter);
+app.use("/api/leave", leaveRouter);
 
 // server connection
 await connectDB();
